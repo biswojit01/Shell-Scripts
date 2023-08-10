5f42fde493da18a65f957211a1960f0f72cc4360
Biswojit Sahoo <126757710+biswojit01@users.noreply.github.com>
2023-08-10 22:52:27 +0530

    Update setup-tomcat.sh

--- setup-tomcat.sh
+++ setup-tomcat.sh
@@ -6,4 +6,8 @@ sudo apt install maven -y
 sudo apt-get install tomcat9 -y
 sudo systemctl start tomcat9
 sudo systemctl enable tomcat9
-
+git clone https://github.com/biswojit01/Train-Ticket-Reservation-System.git
+cd Train-Ticket-Reservation-System
+mvn install
+sudo chmod -R o+wx /var/lib/tomcat9/webapps
+sudo cp -r target/TrainBook-1.0.0-SNAPSHOT.war /var/lib/tomcat9/webapps
